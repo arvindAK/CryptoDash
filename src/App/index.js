d8c95004d527b090a8f0501e02d461781a1e721d
Anurag Kumar <anuragak@users.noreply.github.com>
2019-04-03 12:29:37 +0100

    added styled components

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
+import styled, { css } from "styled-components";
+import Welcome from "./WelcomeMessage";
 
 class App extends Component {
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div>
+        <Welcome />
+      </div>
+    );
   }
 }
 
