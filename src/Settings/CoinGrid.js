20a72cb5eef651ddd7a2f591bbf6994ec6f38d85
Anurag Kumar <anuragak@users.noreply.github.com>
2019-04-03 20:40:05 +0100

    add TopSection Grid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -11,17 +11,17 @@ export const CoinGridStlyed = styled.div`
   margin-top: 40px;
 `;
 
-function getCoinsToDisplay(coinList) {
-  return Object.keys(coinList).slice(0, 100);
+function getCoinsToDisplay(coinList, topSection) {
+  return Object.keys(coinList).slice(0, topSection ? 10 : 100);
 }
 
-export default function() {
+export default function(topSection) {
   return (
     <AppContext.Consumer>
       {({ coinList }) => (
         <CoinGridStlyed>
-          {getCoinsToDisplay(coinList).map((coinKey, index) => (
-            <CoinTile coinKey={coinKey} />
+          {getCoinsToDisplay(coinList, topSection).map((coinKey, index) => (
+            <CoinTile topSection={topSection} coinKey={coinKey} />
           ))}
         </CoinGridStlyed>
       )}
