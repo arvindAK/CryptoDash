5be3f34d58de0d57e4daf222f6356d394e3ceb43
Anurag Kumar <anuragak@users.noreply.github.com>
2019-04-04 17:24:35 +0100

    finish add/remove coins

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { AppContext } from "../App/AppProvider";
-import { SelectableTile } from "../Shared/Tile";
 import CoinTile from "./CoinTile";
 
 export const CoinGridStlyed = styled.div`
@@ -11,16 +10,18 @@ export const CoinGridStlyed = styled.div`
   margin-top: 40px;
 `;
 
-function getCoinsToDisplay(coinList, topSection) {
-  return Object.keys(coinList).slice(0, topSection ? 10 : 100);
+function getCoinsToDisplay(coinList, favorites, topSection) {
+  return topSection.topSection
+    ? favorites
+    : Object.keys(coinList).slice(0, 100);
 }
 
 export default function(topSection) {
   return (
     <AppContext.Consumer>
-      {({ coinList }) => (
+      {({ coinList, favorites }) => (
         <CoinGridStlyed>
-          {getCoinsToDisplay(coinList, topSection).map((coinKey, index) => (
+          {getCoinsToDisplay(coinList, favorites, topSection).map(coinKey => (
             <CoinTile topSection={topSection} coinKey={coinKey} />
           ))}
         </CoinGridStlyed>
--- src/Settings/CoinTile.js
+++ src/Settings/CoinTile.js
@@ -1,20 +1,34 @@
 import React from "react";
 import { AppContext } from "../App/AppProvider";
-import { SelectableTile, DisabledTile, DeletableTile } from "../Shared/Tile";
+import { SelectableTile, DeletableTile, DisabledTile } from "../Shared/Tile";
 import CoinHeaderGrid from "./CoinHeaderGrid";
 import CoinImage from "../Shared/CoinImage";
+
+function clickCoinHandler(topSection, coinKey, addCoin, removeCoin) {
+  return topSection.topSection
+    ? () => {
+        removeCoin(coinKey);
+      }
+    : () => {
+        addCoin(coinKey);
+      };
+}
 
 export default function({ coinKey, topSection }) {
   return (
     <AppContext.Consumer>
-      {({ coinList }) => {
+      {({ coinList, addCoin, removeCoin, isInFavorites }) => {
         let coin = coinList[coinKey];
         let TileClass = SelectableTile;
-        if (topSection) {
+        if (topSection.topSection) {
           TileClass = DeletableTile;
+        } else if (isInFavorites(coinKey)) {
+          TileClass = DisabledTile;
         }
         return (
-          <TileClass>
+          <TileClass
+            onClick={clickCoinHandler(topSection, coinKey, addCoin, removeCoin)}
+          >
             <CoinHeaderGrid
               name={coin.CoinName}
               symbol={coin.Symbol}
