339b924f2a5c70e768a173af457b34ec1164f02c
Anurag Kumar <anuragak@users.noreply.github.com>
2019-04-10 19:44:25 +0100

    added highCharts

---
+++ src/Dashboard/PriceChart.js
@@ -0,0 +1,18 @@
+import HighCharts from "./HighChartsConfig";
+import React from "react";
+import { Tile } from "../Shared/Tile";
+import { AppContext } from "../App/AppProvider";
+import ReactHighcharts from "react-highcharts";
+import HighChartsConfig from "./HighChartsConfig";
+
+export default function() {
+  return (
+    <AppContext.Consumer>
+      {({}) => (
+        <Tile>
+          <ReactHighcharts config={HighChartsConfig()} />
+        </Tile>
+      )}
+    </AppContext.Consumer>
+  );
+}
